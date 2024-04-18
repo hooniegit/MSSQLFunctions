40f9be849afc851d0a5fcd6c07de545a17b0da67
hooniegit <neivekim76@gmail.com>
2024-04-18 09:50:04 +0900

    240418 style: Added explanations

--- SplitBackFunction.sql
+++ SplitBackFunction.sql
@@ -1,4 +1,7 @@
 
+-- CHARINDEX(<column or string>, <input_string>) - 해당 문자열의 시작 위치 반환
+-- SUBSTRING(<column or string>, <start_location_cnt>, <length) - 특정 위치부터 문자열 추출
+
 CREATE FUNCTION SplitBack
 (
     @inputString NVARCHAR(MAX),
--- SplitFrontFunction.sql
+++ SplitFrontFunction.sql
@@ -1,4 +1,7 @@
 
+-- CHARINDEX(<column or string>, <input_string>) - 해당 문자열의 시작 위치 반환
+-- LEFT(<column or string>, <>) - 특정 위치까지 문자열 추출
+
 CREATE FUNCTION SplitFront
 (
     @inputString NVARCHAR(MAX),
